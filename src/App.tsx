6616b4ff9ff080c5afc5194e7828af0a592fbb91
Roi Israeli <roiisra4@gmail.com>
2023-05-01 22:20:28 +0300

    add a custom hook to fetch and clean external users data

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import Layout from './components/UI/Layout/Layout';
-import Header from './components/UI/header/Header';
-import UsersLayout from './components/lib/UsersLayout'
-import styles from'./App.module.scss';
+import React from "react";
+import Layout from "./components/UI/Layout/Layout";
+import Header from "./components/UI/header/Header";
+import UsersLayout from "./components/lib/UsersLayout";
+import mockResponse from "./tmp/MockResponse.json";
+import useFetchUsers from "./hooks/useFetchUsers";
+
+const USERS_URL = "https://randomuser.me/api/?results=10";
 
 function App() {
+  const [users, setUsers] = useFetchUsers(USERS_URL);
   return (
     <Layout>
-      <Header 
-        title='Welcome to the Users Library'
-        image_url='https://cdn-icons-png.flaticon.com/512/666/666201.png'
+      <Header
+        title="Welcome to the Users Library"
+        image_url="https://cdn-icons-png.flaticon.com/512/666/666201.png"
       />
-      <UsersLayout users={[]}/>
+      {users && <UsersLayout users={users} />}
     </Layout>
   );
 }
---
+++ src/hooks/useFetchUsers.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { userType } from "../types";
+
+const userResponseCleaner = (users: any) => {
+  return users.results.map((user: any) => {
+    return {
+      name: `${user.name.first} ${user.name.last}`,
+      email: user.email,
+      image_url: user.picture.medium,
+      location: {
+        country: user.location.country,
+        city: user.location.city,
+        street: `${user.location.street.number}, ${user.location.street.name}`,
+      },
+      uuid: user.id.value,
+    };
+  });
+};
+
+export default function useFetchUsers(url: string): [userType[], Function] {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    let response;
+    try {
+      response = await fetch(url);
+    } catch (e) {
+      console.log("Could not fetch users...");
+    }
+    const users = userResponseCleaner(await response?.json());
+    setUsers(users);
+  }
+
+  return [users, setUsers];
+}
